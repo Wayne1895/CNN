06953c752239addd453eb2d353d021a5c2913b3b
Wayne1895 <waynes1895@gmail.com>
2024-09-04 21:02:55 +0800

    09/04

--- dense.py
+++ dense.py
@@ -12,5 +12,8 @@ class Dense(Layer):
         return np.dot(self.weights, self.input) + self.bias
     
     def backward(self, output_gradient, learning_rate):
-        weights_gradient = 
+        weights_gradient = np.dot(output_gradient, self.input.T)
+        self.weights -= learning_rate * weights_gradient
+        self.bias -= learning_rate * output_gradient
         
+        return np.dot(self.weights.T, output_gradient)
