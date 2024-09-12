48cb8aab109278a95f51c7ccb072169872ee9b49
Wayne1895 <waynes1895@gmail.com>
2024-09-12 23:38:03 +0800

    first commit

--- convolution_layer.py
+++ convolution_layer.py
@@ -8,5 +8,14 @@ class Convolution(Layer):
         self.depth = depth
         self.input_shape = input_shape
         self.input_depth = input_depth
+        self.output_shape = (depth, input_height - kernel_size + 1, input_width - kernel_size + 1)
+        self.kernels_size = (depth, input_depth, kernel_size, kernel_size)
+        self.kernels = np.random.randn(*self.kernels_size)
+        self.bias = np.random.randn(*self.output_shape)
+    
+    def forward(self, input):
+        pass
         
-         
+    def backward(self, output_gradient, learning_rate):
+        pass
+        
